1651f5fb8f574386d30f9c2cc1f22b369a038e61
Grigorov Alexandr <lcpatron@yandex.ru>
2021-03-15 06:00:48 +0300

    Sorts array of string on "ru" and "en" locales with two criteria "asc" or "desc".

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,37 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    const resultArray = [...arr];
 
+    function SortingException(message) {
+        this.message = message;
+        this.name = "Ошибка сортировки";
+     }
+
+    function compareString(str1, str2){
+        const regexpRus = /[А-яЁё]/i;
+
+        const compareOptions = { 
+            numeric: true,
+            caseFirst: 'upper' 
+        };
+
+        if(regexpRus.test(str1)){
+            return str1.localeCompare(str2, 'ru', compareOptions);
+        }else{
+            return str1.localeCompare(str2, 'en', compareOptions);
+        }
+    }
+
+    if(!Array.isArray(arr))
+        throw new SortingException("Переданный аргумент не является массивом");
+
+    switch(param){
+        case 'asc':
+            return resultArray.sort(compareString);;
+        case 'desc':
+            return resultArray.sort(compareString).reverse();
+        default:
+            throw new SortingException("Неверный параметр сортировки");
+    }
 }

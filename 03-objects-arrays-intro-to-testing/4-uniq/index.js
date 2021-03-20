bd59fb19dc473399e2e4bb573ef7db8bac247ef6
Grigorov Alexandr <lcpatron@yandex.ru>
2021-03-21 01:52:48 +0300

    uniq function

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,29 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+    function UniqException(message) {
+        this.message = message;
+        this.name = "Ошибка функции uniq";
+    }
 
+    if(arr === undefined){
+        return [];
+    }
+
+    if(!Array.isArray(arr))
+        throw new UniqException("Переданный аргумент не является массивом");
+
+    if(arr.length === 0){
+        return [];
+    }
+
+    let resultArray = [];
+    
+    arr.forEach((item) => {
+        if(resultArray.indexOf(item) === -1){
+            resultArray.push(item);
+        }
+    })
+
+    return resultArray;
 }

3180e7914b294a9db340d408176ba89a5712cda0
Grigorov Alexandr <lcpatron@yandex.ru>
2021-03-21 00:50:20 +0300

    create-getter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,39 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    function CreateGetterException(message) {
+        this.message = message;
+        this.name = "Ошибка создания геттера";
+    }
 
+    if (typeof path !== 'string')
+        throw new CreateGetterException("Неверный параметр функции createGetter");
+
+    const objectKeys = path.split('.');
+
+    return function (object) {
+        function FindValueException(message) {
+            this.message = message;
+            this.name = "Ошибка поиска значения в объекте";
+        }
+
+        if (typeof object !== 'object')
+            throw new FindValueException("Неверный параметр функции получения значения");
+
+        function findValue(object, index) {
+            for (const [key, value] of Object.entries(object)) {
+                if (typeof object[objectKeys[index]] === 'object') {
+                    let result = findValue(object[objectKeys[index]], index + 1);
+
+                    if (result !== undefined) {
+                        return result;
+                    }
+                } else if (key === objectKeys[index]) {
+                    return value;
+                }
+            }
+        }
+
+        return findValue(object, 0);
+    }
 }

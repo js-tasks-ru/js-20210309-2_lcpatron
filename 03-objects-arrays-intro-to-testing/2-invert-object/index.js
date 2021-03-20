93b8a1c64700dde2769a1aeb3ed20e4a3a7c3cc5
Grigorov Alexandr <lcpatron@yandex.ru>
2021-03-21 01:00:08 +0300

    inver object function

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,22 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    function InvertObjectException(message) {
+        this.message = message;
+        this.name = "Ошибка функции invertObj";
+    }
 
+    if(obj === undefined)
+        return undefined;
+
+    if (typeof obj !== 'object')
+        throw new InvertObjectException("Неверный параметр функции invertObj");
+
+    const result = {};
+
+    for (const [key, value] of Object.entries(obj)){
+        result[value] = key;
+    }
+
+    return result;
 }

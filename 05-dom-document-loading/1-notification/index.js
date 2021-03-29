dde832a321a1788ff86943a2033d3ddc0eb95398
Grigorov Alexandr <lcpatron@yandex.ru>
2021-03-29 20:19:03 +0300

    notification message

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,76 @@
 export default class NotificationMessage {
+    static visible = false;
 
+    static isVisible(){
+        return NotificationMessage.visible;
+    }
+
+    static setVisible(visible){
+        NotificationMessage.visible = visible;
+    }
+
+    constructor(message = 'Hello world!', { duration, type } = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+
+        this.render();
+        this.initEventListeners();
+    }
+
+    getDurationInSec(duration) {
+        return duration / 1000;
+    }
+
+    getTemplate() {
+        return (
+            `<div class="notification ${this.type}" style="--value:${this.getDurationInSec(this.duration)}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.message}
+                    </div>
+                </div>
+            </div>`
+        );
+    }
+
+    render() {
+        const element = document.createElement('div'); // (*)
+
+        element.innerHTML = this.getTemplate();
+
+        this.element = element.firstElementChild;
+    }
+
+    show(renderingElement = undefined) {
+        if (!NotificationMessage.isVisible()) {
+            NotificationMessage.setVisible(true);
+        }else{
+            document.querySelector('.notification').remove();
+        }
+        
+        if (renderingElement !== undefined) {
+            renderingElement.append(this.element);
+        } else {
+            document.body.append(this.element);
+        }
+
+        setTimeout(() => {
+            this.remove();
+            NotificationMessage.setVisible(false);
+        }, this.duration);
+    }
+
+    initEventListeners() {
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+    }
 }

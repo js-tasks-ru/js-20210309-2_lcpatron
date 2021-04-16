5cab17ea6c886bf3a3245e2c920cf085e49db986
Grigorov Alexandr <lcpatron@yandex.ru>
2021-04-16 10:28:50 +0300

    SortableList with basic functionality

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,28 @@
 export default class SortableList {
+    constructor({ items = [] } = {}) {
+        this.items = items;
 
+        this.render();
+    }
+
+    getTemplate(items) {
+        return `
+            <ul class="sortable-list">
+                ${items.map(item => {
+                    item.classList.add('sortable-list__item');
+                    return item.outerHTML
+                }).join('')}
+            </ul>
+        `;
+    }
+
+    render() {
+        const wrapper = document.createElement('div');
+
+        wrapper.innerHTML = this.getTemplate(this.items);
+
+        const element = wrapper.firstElementChild;
+
+        this.element = element;
+    }
 }
